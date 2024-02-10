c8066af92dbbe6d8af267bf5829679fcb21186ac
Xerkse <cjmp9876@gmail.com>
2024-02-10 12:06:26 -0600

    font change

--- config.h
+++ config.h
@@ -6,7 +6,10 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = {"JoyPixels:pixelsize=20:antialias=true:autohint=true"};
+static char *font2[] = {
+	//"JoyPixels:pixelsize=20:antialias=true:autohint=true"
+	"NotoColorEmoji:pixelsize=20:antialias=true:autohint=true"
+};
 static int borderpx = 2;
 
 /*
